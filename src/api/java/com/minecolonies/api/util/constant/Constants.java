3e8e6003b40c598363023e86b3c5aa1f33896ac7
Thomas McManus <mcmanustfj@gmail.com>
2020-04-26 11:37:56 -0400

    Added glowing on damage taken

--- src/api/java/com/minecolonies/api/util/constant/Constants.java
+++ src/api/java/com/minecolonies/api/util/constant/Constants.java
@@ -153,6 +153,7 @@ public final class Constants
     public static final Effect GLOW_EFFECT       = Effects.GLOWING;
 
     public static final int GLOW_EFFECT_DURATION   = 20 * 30;
+    public static final int GLOW_EFFECT_DAMAGE_DURATION   = 20 * 15;
     public static final int GLOW_EFFECT_MULTIPLIER = 20;
     public static final int GLOW_EFFECT_DISTANCE   = 60;
 
